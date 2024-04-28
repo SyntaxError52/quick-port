c1e796030c7a1d3d3a9cd61329dcb334315e843f
SyntaxError52 <160362207+SyntaxError52@users.noreply.github.com>
2024-04-28 16:37:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeTheme(theme) {
+    var findlink = document.getElementsByTagName("link")[0];
+    if (theme === 'night') {
+        findlink.href = "themes/night.css";
+    } else if (theme === 'blue') {
+        findlink.href = "themes/blue.css";
+    }
+}
